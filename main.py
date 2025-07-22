7e9d4c6d2a245ab74011dc7c281c1ff3002a8e6c
ETchaleu <etchaleungankeu@aneo.fr>
2025-07-22 11:13:51 +0200

    Analyser done, consistent conjugation structure, corrector execpt if correction

--- main.py
+++ main.py
@@ -41,10 +41,11 @@ def finalSay(res, i):
 
 # Problème d'implémentation de la stochasticité, en effet les 1 minimaux d'un période ne failent pas forcément à la suivante il faut un cache ou alors transmette le fait que ce truc ne marche pas
 Viz = MultiViz(active=printgraph)
-### config = controllers.TestConfig([[[56], 0.3], [[94], 0.3], [[42, 40], 0.5], [[118, 114, 115], 0.5], [[76, 80, 82], 0.5]]) EXCELLENT EXEMPLE avec N = 2**7
+#config = controllers.TestConfig([[[56], 0.3], [[94], 0.3], [[42, 40], 0.5], [[118, 114, 115], 0.5], [[76, 80, 78, 82], 0.5]])# EXCELLENT EXEMPLE avec N = 2**7
+#config = controllers.TestConfig([[[0, 2, 4, 6], 0.5]])
 config = controllers.TestConfig([([51], 0.3), ([1], 0.3), ([12, 11], 0.5), ([4, 2, 0], 0.5), ([63, 62, 61], 0.5), ([15, 10, 6], 0.5), ([20, 17, 16], 0.5), ([46, 40, 35], 0.5)])
 config.setMode("Analyse")
-#config.GenProb(N, (2, 1, 0, 0.3), (1, 2, 2, 0.5), (5, 3, 4, 0.5)) # (combien, taille, écart type)
+#config.GenProb(N, (2, 1, 0, 0.3), (1, 2, 2, 0.5), (6, 3, 4, 0.5)) # (combien, taille, écart type)
 nbRunTab = [1, 4, 6]
 print(config.Pb)
 input("press to continue...")
